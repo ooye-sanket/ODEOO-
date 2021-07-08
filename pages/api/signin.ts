3e7876020c053152edaa010eda855d5c88259230
Vraj Shah <thevrajshah@gmail.com>
2021-07-09 01:10:55 +0530

    Fix: SignUp error

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -9,24 +9,36 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
 	switch (req.method) {
 		case 'POST':
-			if (!req.body) {
-				res.statusCode = 404;
-				res.end('Error');
-				return;
+			if (!req.body) return res.status(400).json({ error: 'Request Invalid' });
+
+			if (
+				!req.body.username ||
+				!req.body.email ||
+				!req.body.phone ||
+				!req.body.password
+			)
+				return res
+					.status(400)
+					.json({ error: 'Please enter all the required fields' });
+
+			if (req.body.email) {
+				const { email, password } = req.body;
+				try {
+				} catch (err) {}
 			}
-			const { username, password } = req.body;
+
 			res.json({
 				token: jwt.sign(
 					{
 						username,
 					},
+					// @ts-ignore
 					process.env.JWT_SECRET
 				),
 			});
 			break;
 		default:
-			res.status(405).end(); //Method Not Allowed
-			break;
+			return res.status(405).end('Method Not Allowed'); //Method Not Allowed
 	}
 };
 
--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -81,26 +81,32 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 					meta,
 				});
 
-				const createdUsr = await newUsr
-					.save()
-					.then((r) => console.log(r))
-					.catch((e) => console.error(e));
+				const createdUsr = await newUsr.save();
 
 				if (createdUsr)
 					return res.status(200).json({
-						token: jwt.sign(
-							createdUsr,
-							// @ts-ignore
-							process.env.JWT_SECRET
-						),
+						message: 'User signed up successfully!',
 						username,
 						email,
 						phone,
+						token: jwt.sign(
+							{ user: createdUsr },
+							// @ts-ignore
+							process.env.JWT_SECRET,
+							{ expiresIn: '15d' }
+						),
 					});
-			} catch (error) {
-				console.error('SignUp Error:');
+			} catch (err) {
+				console.error('SignUp Error:', err);
 				return res.status(500).json({ error: 'Something went wrong' });
 			}
+			break;
+		// CheckEmail.then()
+		// CheckPhone,then()
+		// CheckUname,then()
+		// CheckAadhar.then()
+
+		// HashPwd.then(newUsr).then(saveuser)
 
 		default:
 			return res.status(405).end('Method Not Allowed'); //Method Not Allowed
@@ -108,3 +114,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 export default connectDB(handler);
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+// 	.eyJ1c2VyIjp7Im1ldGEiOnsiZ2VucmUiOlsiQm9sbHl3b29kIl0sImV2ZW50cyI6WyJSaW5nIENlcmVtb255Il19LCJ5b3V0dWJlTGlua3MiOltdLCJyb2xlIjoiVU5WRVJJRklFRF9BUlRJU1QiLCJfaWQiOiI2MGU3NTA2NGYzNjAyMjBlMjA4MTUyZDciLCJmdWxsTmFtZSI6IkpvaG4gRG9lIiwidXNlcm5hbWUiOiJqb2huZG9lIiwiZW1haWwiOiJhYmNAeHl6LmNvbSIsInBob25lIjoiKzkxOTg3NjU0MzIxMCIsInBhc3N3b3JkIjoiJDJiJDEyJGszQ243aWVKUU5NeW9RRW5QSC4uTk9VSGYweHAwQ3I2bjJOck9INjVOZmlXLkhxTVVRaGNXIiwiZGF0ZU9mQmlydGgiOiIyMC0wNS0yMDAwIiwiYWRkcmVzcyI6IllvdXIgSGVhcnQiLCJpbWFnZVVybCI6Imh0dHBzOiAvL3BpY3N1bS5waG90b3MvMjAwIiwiY29udGVudFRvRGlzcGxheSI6W3siX2lkIjoiNjBlNzUwNjRmMzYwMjIwZTIwODE1MmQ4IiwidGl0bGUiOiJZb3V0dWJlIFZpZGVvIiwiZGVzY3JpcHRpb24iOiJNeSBZb3V0dWJlIFZpZGVvIiwibWVkaWFVcmwiOiJodHRwczogLy9waWNzdW0ucGhvdG9zLzIwMCJ9XSwiY3JlYXRlZEF0IjoiMjAyMS0wNy0wOFQxOToyMjoxMi40NzFaIiwidXBkYXRlZEF0IjoiMjAyMS0wNy0wOFQxOToyMjoxMi40NzFaIiwiX192IjowfSwiaWF0IjoxNjI1NzcyMTMyLCJleHAiOjE2MjcwNjgxMzJ9
+// 	.d1EB6ij0tmWnRptGUGn23mE_QvmAoc7Xow2QOxHH7k;
