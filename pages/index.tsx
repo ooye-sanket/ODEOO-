a6fc456ec789a393714b0b52b60995e4d8287977
Vraj Shah <thevrajshah@gmail.com>
2021-07-24 21:23:49 +0530

    Fix: Hero section responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 const Home = () => {
 
@@ -8,12 +8,15 @@ const Home = () => {
 		<Container fluid className='bg-light'>
 			<Container>
 				<Row id='hero-section' className='align-items-center'>
-					<Col >
+					<Col xs={{ span: 12, order: 'last' }} lg='6'>
 						<div className='display-4'>Welcome to Odeo.in</div>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa distinctio, praesentium nam odio animi nobis voluptatum repellendus explicabo, sed at molestiae fugiat dolorem, consectetur ipsum accusamus a ex! Minus, perspiciatis.</p>
+						<Button>Post a Requirement</Button>
 					</Col>
-					<Col>
-						<img src='/rock-band.svg' alt='' />
+					<Col xs={{ span: 12, order: 'first' }} lg='6'>
+						<div className="text-center">
+							<img src='/rock-band.svg' alt='' />
+						</div>
 					</Col>
 				</Row>
 			</Container>
