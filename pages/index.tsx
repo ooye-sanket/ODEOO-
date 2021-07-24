f987fc3139b7da1349bc62ee52c2b3218990d9dd
Vraj Shah <thevrajshah@gmail.com>
2021-07-24 21:08:32 +0530

    Feat: Hero Section created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Home = () => {
 
 	return (
-		<>
-			hi
-
-		</>
+		<Container fluid className='bg-light'>
+			<Container>
+				<Row id='hero-section' className='align-items-center'>
+					<Col >
+						<div className='display-4'>Welcome to Odeo.in</div>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa distinctio, praesentium nam odio animi nobis voluptatum repellendus explicabo, sed at molestiae fugiat dolorem, consectetur ipsum accusamus a ex! Minus, perspiciatis.</p>
+					</Col>
+					<Col>
+						<img src='/rock-band.svg' alt='' />
+					</Col>
+				</Row>
+			</Container>
+		</Container >
 	);
 }
 
