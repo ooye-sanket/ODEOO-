50011c3915e89eff23d814d805144eb6320c344e
Vraj Shah <thevrajshah@gmail.com>
2021-07-28 20:03:25 +0530

    Fix: User verification mech. changed

--- seeders.ts
+++ seeders.ts
@@ -30,14 +30,6 @@ const user1 = {
 	address: 'Your Heart',
 	aadhar: '123445679876',
 	imageUrl: 'https://picsum.photos/200',
-	role: Role.UnverifiedArtist,
-	contentToDisplay: [
-		{
-			title: 'Youtube Video',
-			description: 'My Youtube Video',
-			mediaUrl: 'https://picsum.photos/200',
-		},
-	],
 	youtubeLinks: ['https://picsum.photos/200', 'https://picsum.photos/200'],
 	meta: {
 		genre: ['Bollywood'],
