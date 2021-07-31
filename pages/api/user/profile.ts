d5ac6a0e959c50b20d1bbb4484c8be4a87ec8571
Vraj Shah <thevrajshah@gmail.com>
2021-07-31 17:10:32 +0530

    Feat: Profile edit API

--- pages/api/user/profile.ts
+++ pages/api/user/profile.ts
@@ -71,8 +71,58 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 				console.error('Profile Update Error:', err);
 				return res.status(500).json({ msg: 'Something went wrong' });
 			}
-			// }
 			break;
+
+		case 'PUT':
+			if (
+				!id ||
+				!firstName ||
+				!lastName ||
+				!username ||
+				!email ||
+				!phone ||
+				!address ||
+				!dateOfBirth ||
+				!aadhar ||
+				!imgUrl ||
+				!youtubeLinks ||
+				!meta
+			)
+				return res
+					.status(400)
+					.json({ msg: 'Please enter all the required fields' });
+
+			try {
+				const usr = await User.findById(id).select('id password');
+				compare(password, usr.password, async (err: any, obj: any) => {
+					if (!err && obj) {
+						const updatedUsr = await User.findByIdAndUpdate(usr.id, {
+							firstName,
+							lastName,
+							username,
+							email,
+							phone,
+							address,
+							dateOfBirth,
+							aadhar,
+							imgUrl,
+							youtubeLinks,
+							meta,
+						}).select(
+							'address dateOfBirth aadhar imgUrl youtubeLinks meta.genre meta.events'
+						);
+						return res.status(200).json({
+							msg: 'Profile created successfully',
+							data: updatedUsr,
+						});
+					} else {
+						return res.status(401).json({ msg: 'Password invalid' });
+					}
+				});
+			} catch (err: any) {
+				console.error('Profile Update Error:', err);
+				return res.status(500).json({ msg: 'Something went wrong' });
+			}
 		default:
 			return res.status(405).end('Method Not Allowed'); //Method Not Allowed
 	}
