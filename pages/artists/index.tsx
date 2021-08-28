3ce1bc6cefab4ef0010baf816990220e06bac74a
Vraj Shah <thevrajshah@gmail.com>
2021-08-28 23:45:06 +0530

    Fix: ESLint errors

--- pages/artists/index.tsx
+++ pages/artists/index.tsx
@@ -4,9 +4,9 @@ const Artists = () => {
    return (
       <header className="py-3 mb-4 border-bottom">
          <Container className='d-flex flex-wrap justify-content-center'>
-            <a href="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+            <span className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
                <span className="fs-4">Find the Best Artists</span>
-            </a>
+            </span>
             <form className="col-12 col-lg-auto mb-3 mb-lg-0">
 
 
